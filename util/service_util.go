7396e8b8ec83f33c588e86f7ef3604c9ffd8df26
Jeremiah Hasudungan <jeremiah.sihombing97@gmail.com>
2022-07-07 15:00:40 +0700

    fix : service util to not wrapped in struct

--- util/service_util.go
+++ util/service_util.go
@@ -5,9 +5,7 @@ import (
 	"time"
 )
 
-type ServiceUtil struct{}
-
-func (s *ServiceUtil) GenerateId(key string) string {
+func GenerateId(key string) string {
 
 	now := time.Now().Format("20060102150405")
 	id := fmt.Sprintf("%v%v", key, now)
--- util/service_util_test.go
+++ util/service_util_test.go
@@ -9,8 +9,7 @@ func TestServiceUtil(t *testing.T) {
 
 	t.Run("TestGenerateId", func(t *testing.T) {
 
-		serviceUtil := &ServiceUtil{}
-		id := serviceUtil.GenerateId("STR")
+		id := GenerateId("STR")
 		log.Printf("Id : %v", id)
 
 	})
