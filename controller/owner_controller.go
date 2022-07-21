621c3b117e521d4390fbc9f3eb040e753c5118a1
Jeremiah Hasudungan <jeremiah.sihombing97@gmail.com>
2022-07-21 18:04:23 +0700

    feat : store management basic ui functionality

--- controller/owner_controller.go
+++ controller/owner_controller.go
@@ -1,11 +1,14 @@
 package controller
 
 import (
+	"alingan/core/model"
 	"alingan/core/service"
 	"html/template"
 	"log"
 	"net/http"
 	"path"
+
+	"github.com/gorilla/mux"
 )
 
 type OwnerController struct {
@@ -38,5 +41,139 @@ func (o *OwnerController) ShowStoreData(w http.ResponseWriter, r *http.Request) 
 	templateData["stores"] = stores
 
 	err = template.Execute(w, templateData)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+}
+
+func (o *OwnerController) ShowStoreInformation(w http.ResponseWriter, r *http.Request) {
+
+	// storeId
+	params := mux.Vars(r)
+	storeId := params["storeId"]
+
+	store, err := o.StoreService.FindStoreById(storeId)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	template, err := template.ParseFiles(path.Join("view", "owner/view_store.html"), path.Join("view", "layout/owner_layout.html"))
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	templateData := make(map[string]interface{})
+
+	templateData["store"] = store
+
+	err = template.Execute(w, templateData)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+}
+
+func (o *OwnerController) ShowCreateStoreForm(w http.ResponseWriter, r *http.Request) {
+
+	template, err := template.ParseFiles(path.Join("view", "owner/create_store.html"), path.Join("view", "layout/owner_layout.html"))
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	err = template.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+}
+
+func (o *OwnerController) HandleCreateStoreFormRequest(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	request := model.CreateStoreRequest{}
+	// we get owner id from sessions
+	request.OwnerId = "owner-001"
+	request.StoreName = r.Form.Get("store-name")
+	request.StoreAddress = r.Form.Get("store-address")
+
+	err = o.StoreService.CreateStore(request)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	http.Redirect(w, r, "/owner/store", http.StatusSeeOther)
+
+}
+
+func (o *OwnerController) HandleInactiveStoreRequest(w http.ResponseWriter, r *http.Request) {
+
+	// storeId
+	params := mux.Vars(r)
+	storeId := params["storeId"]
+
+	err := o.StoreService.SetStoreInactive(storeId)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	http.Redirect(w, r, "/owner/store", http.StatusSeeOther)
+
+}
+
+func (o *OwnerController) HandleUpdateStoreRequest(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	request := model.UpdateStoreRequest{}
+	request.StoreAddress = r.Form.Get("update-store-address")
+	request.StoreName = r.Form.Get("update-store-name")
+
+	err = o.StoreService.UpdateStore(request, r.Form.Get("store-id"))
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Something Went Wrong - Exceute Render", 500)
+		return
+	}
+
+	http.Redirect(w, r, "/owner/store", http.StatusSeeOther)
 
 }
--- server.go
+++ server.go
@@ -31,7 +31,12 @@ func main() {
 	// router and handler
 	r := &mux.Router{}
 	r.HandleFunc("/", publicController.ShowIndexPage)
-	r.HandleFunc("/owner/store", ownerController.ShowStoreData)
+	r.HandleFunc("/owner/store", ownerController.ShowStoreData).Methods("GET")
+	r.HandleFunc("/owner/store/{storeId}", ownerController.ShowStoreInformation).Methods("GET")
+	r.HandleFunc("/owner/new/store", ownerController.ShowCreateStoreForm).Methods("GET")
+	r.HandleFunc("/owner/new/store/submit", ownerController.HandleCreateStoreFormRequest).Methods("POST")
+	r.HandleFunc("/owner/inactive/store/{storeId}", ownerController.HandleInactiveStoreRequest).Methods("GET")
+	r.HandleFunc("/owner/update/store/submit", ownerController.HandleUpdateStoreRequest).Methods("POST")
 
 	// file server
 	assetFileServer := http.FileServer(http.Dir("asset"))
