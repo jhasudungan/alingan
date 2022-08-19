216cb037d19fcf917caa4a2b5fdda3682d7cc85f
Jeremiah Hasudungan <jeremiah.sihombing97@gmail.com>
2022-08-19 16:41:17 +0700

    feat : db now use env

--- config/db.go
+++ config/db.go
@@ -2,13 +2,33 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func CreateDBConnection() (*sql.DB, error) {
 
-	connectionString := "host=localhost port=5432 user=postgres password=root dbname=alingan sslmode=disable"
+	err := godotenv.Load()
+
+	if err != nil {
+		return nil, err
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbHostPort := os.Getenv("DB_HOST_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connectionString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable",
+		dbHost,
+		dbHostPort,
+		dbUser,
+		dbPass,
+		dbName)
 
 	con, err := sql.Open("postgres", connectionString)
 
