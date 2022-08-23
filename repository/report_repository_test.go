d4e02957fd3633598abbdca14a91d4def8f02114
Jeremiah Hasudungan <jeremiah.sihombing97@gmail.com>
2022-08-23 16:37:21 +0700

    fix : report repo test code

--- repository/report_repository_test.go
+++ repository/report_repository_test.go
@@ -8,15 +8,19 @@ import (
 
 func TestReportRepository(t *testing.T) {
 
+	/**
+	- run alingan-test-source-script.sql on "core" schema before run below test
+	- .env need to be present in "repository" package in order to run go test
+	*/
+
 	reportRepo := ReportRepositoryImpl{}
 
 	t.Run("TestOwnerMostPurchasedProductByQuantity", func(t *testing.T) {
 
 		results, _ := reportRepo.FindOwnerMostPurchasedProductByQuantity("owner-001")
 
-		assert.Equal(t, "PRDa543809e-f36f-443a-a815-64c0e2f0e09c", results[0].ProductId)
-		assert.Equal(t, "prd-002", results[1].ProductId)
-		assert.Equal(t, "prd-001", results[2].ProductId)
+		assert.Equal(t, "prd-002", results[0].ProductId)
+		assert.Equal(t, "prd-001", results[1].ProductId)
 
 	})
 
@@ -25,8 +29,7 @@ func TestReportRepository(t *testing.T) {
 		results, _ := reportRepo.FindOwnerMostPurchasedProductByRevenue("owner-001")
 
 		assert.Equal(t, "prd-002", results[0].ProductId)
-		assert.Equal(t, "PRDa543809e-f36f-443a-a815-64c0e2f0e09c", results[1].ProductId)
-		assert.Equal(t, "prd-001", results[2].ProductId)
+		assert.Equal(t, "prd-001", results[1].ProductId)
 
 	})
 
@@ -35,7 +38,6 @@ func TestReportRepository(t *testing.T) {
 		results, _ := reportRepo.FindOwnerStoreWithTheMostTransaction("owner-001")
 
 		assert.Equal(t, "str-001", results[0].StoreId)
-		assert.Equal(t, "str-002", results[1].StoreId)
 
 	})
 
@@ -44,7 +46,6 @@ func TestReportRepository(t *testing.T) {
 		results, _ := reportRepo.FindOwnerAgentWithTheMostTransaction("owner-001")
 
 		assert.Equal(t, "agent-001", results[0].AgentId)
-		assert.Equal(t, "AGT454497b9-74d1-4bb0-8753-962a962e31f6", results[1].AgentId)
 
 	})
 }
